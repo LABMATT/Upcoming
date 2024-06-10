3ef93c44f8b75c4a2c84a00ae476690ce05025b6
Matthew Lewington <matt42764@gmail.com>
2024-06-11 03:25:33 +1000

    Fixed Missing countdown
    - Added the words IN and Days to countdown in entry render

--- assets/scripts/EntryBuilder/EB_eventTodayCountdown.js
+++ assets/scripts/EntryBuilder/EB_eventTodayCountdown.js
@@ -2,15 +2,31 @@
 function EB_eventTodayCountdown(jsonData) {
     
     // Create Div for the eventToday-countdown
-    const eventDivCountdown = document.createElement("div");
-    eventDivCountdown.classList.add("eventToday-countdown");
+    const eventDiv = document.createElement("div");
+    eventDiv.classList.add("eventToday-countdown");
+
+
+    // Creates the word IN at the top of the countdown.
+    const countdownIN = document.createElement("p");
+    const countdownINtxt = document.createTextNode("IN");
+    countdownIN.classList.add("subevent-text-complmenty");
+    countdownIN.append(countdownINtxt);
+    eventDiv.append(countdownIN);
     
-    // Create p for the date
+    // Create number of days till event.
     const subEventTextMain = document.createElement("h2");
-    const subEventTextMainText = document.createTextNode(jsonData.data.date);
+    const subEventTextMainText = document.createTextNode("0");
     subEventTextMain.classList.add("subevent-text-main");
     subEventTextMain.append(subEventTextMainText);
-    eventDivCountdown.append(subEventTextMain);
+    eventDiv.append(subEventTextMain);
+
     
-    return eventDivCountdown;
+    // Creates the word "days" at the bottom of the countdown.
+    const countdownDays = document.createElement("p");
+    const countdowndDaystext = document.createTextNode("Days");
+    countdownDays.classList.add("subevent-text-complmenty");
+    countdownDays.append(countdowndDaystext);
+    eventDiv.append(countdownDays);
+
+    return eventDiv;
 }
