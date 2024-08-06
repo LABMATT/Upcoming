84155e451ca456b907bfec04cbcfc293ae0e534a
Matthew Lewington <matt42764@gmail.com>
2024-08-06 20:11:27 +1000

    Modify transport

--- JAVA/UpcomingServer/src/main/java/space/labmatt/Interface/EditOrg.java
+++ JAVA/UpcomingServer/src/main/java/space/labmatt/Interface/EditOrg.java
@@ -1,20 +1,23 @@
 package space.labmatt.Interface;
 
 import space.labmatt.Transport.Sessions;
+import space.labmatt.Transport.Struts.Organization;
 
+import java.util.List;
 import java.util.Scanner;
 
 public class EditOrg {
 
     public EditOrg(Sessions sessions) {
+        // Asks you to select a current orgination with ID then if valid sends you to an editor.
 
         Scanner input = new Scanner(System.in);
+        List<Organization> organizationSessions = sessions.getOrganizationSessions();
 
         boolean editOrgRunning = true;
         while (editOrgRunning) {
 
-            listOrganizations();
-
+            System.out.println(" ");
             System.out.println("Edit Org > Enter Organization ID number from list.");
 
             new ListOrgs(sessions);
@@ -24,20 +27,33 @@ public class EditOrg {
 
             String inputline = input.nextLine();
 
-            switch (inputline) {
-                case "exit":
-                    editOrgRunning = false;
-                    break;
+            if(inputline.equals("exit")) {
+
+                editOrgRunning = false;
+                break;
             }
+
+            Organization workingOrg = null;
+
+            for(Organization org : organizationSessions) {
+
+                if (org.orgID.equals(inputline)) {
+
+                    workingOrg = org;
+
+                    System.out.println("Found Origination (" + workingOrg.orgNAME + ").");
+
+                    break;
+                }
+            }
+
+            if(workingOrg == null) {
+
+                System.out.println("No Origination by ID (" + inputline + ") found.");
+            }
+
         }
 
         System.out.println("=== END OF EDITING MENU ===");
-    }
-
-
-    private void listOrganizations() {
-
-
-
     }
 }
